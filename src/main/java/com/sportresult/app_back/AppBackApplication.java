483e3cdd54034e03d14c38a68f9b56b6da822b12
ludovicgarnier1979 <ludovicgarnier1979@gmail.com>
2025-07-02 11:00:36 +0200

    init_commit

---
+++ src/main/java/com/sportresult/app_back/AppBackApplication.java
@@ -0,0 +1,13 @@
+package com.sportresult.app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBackApplication.class, args);
+	}
+
+}
